3ae52a742b8167536758176f0437cb16db771b42
yveega <47260215+yveega@users.noreply.github.com>
2021-11-13 17:45:20 +0300

    Upgrade filters.js and change smth in filters.html

--- filters.js
+++ filters.js
@@ -2,5 +2,17 @@ document.querySelector("#showButton").onclick = function() {
     let town = $("#townChoose").val()
     let showNew = $("#orderTypeChoose").val() == "all" || $("#orderTypeChoose").val() == "new"
     let showCanceled = $("#orderTypeChoose").val() == "all" || $("#orderTypeChoose").val() == "canceled"
-    alert(`Town choosed: ${town};\n showNew: ${showNew}; showCanceled: ${showCanceled}`)
+    let showLate = $("#showLateOrders").is(":checked")
+    let minSumOrder = Number($("#orderSum").val())
+    let promo = $("#promoInput").val()
+    let dateInterval = $("[name='datefilter']").val().split(" - ")
+    let startDate = new Date(dateInterval[0])
+    let endDate = new Date(dateInterval[1])
+    alert(
+        `Town choosed: ${town}
+        showNew: ${showNew}; showCanceled: ${showCanceled}
+        showLate: ${showLate}
+        minSum: ${minSumOrder}
+        promo: ${promo}
+        dateInterval: ${startDate} - ${endDate}`)
 }
