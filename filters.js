37117fbb8caa1840850353c3b63e9281d19b2b5d
yveega <47260215+yveega@users.noreply.github.com>
2021-11-13 18:44:13 +0300

    add filter function in filters.js

--- filters.js
+++ filters.js
@@ -1,3 +1,9 @@
+
+function parseDate(dateString) {
+    let [day, month, year] = dateString.split('.')
+    return new Date(year, month - 1, day)
+}
+
 document.querySelector("#showButton").onclick = function() {
     let town = $("#townChoose").val()
     let showNew = $("#orderTypeChoose").val() == "all" || $("#orderTypeChoose").val() == "new"
@@ -6,8 +12,8 @@ document.querySelector("#showButton").onclick = function() {
     let minSumOrder = Number($("#orderSum").val())
     let promo = $("#promoInput").val()
     let dateInterval = $("[name='datefilter']").val().split(" - ")
-    let startDate = new Date(dateInterval[0])
-    let endDate = new Date(dateInterval[1])
+    let startDate = parseDate(dateInterval[0])
+    let endDate = parseDate(dateInterval[1])
     alert(
         `Town choosed: ${town}
         showNew: ${showNew}; showCanceled: ${showCanceled}
@@ -15,4 +21,14 @@ document.querySelector("#showButton").onclick = function() {
         minSum: ${minSumOrder}
         promo: ${promo}
         dateInterval: ${startDate} - ${endDate}`)
+    
+    function filterOrder(ord) {
+        if (!showLate && ord.lated) return false
+        if (ord.sum < minSumOrder) return false
+        if (!showNew && ord.status == 1) return false
+        if (!showLate && ord.status == Late) return false
+        if (promo != "" && ord.promo != promo) return false
+        let date = new Date(ord.date)
+        if (date < startDate || date > endDate) return false
+    }
 }
