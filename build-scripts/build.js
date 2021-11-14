a84dfa13ca79e46a03f2189c0775499ce45367c2
nebolax <alexey.nebolsin.jr@gmail.com>
2021-11-14 23:18:58 +0300

    commented build

--- build-scripts/build.js
+++ build-scripts/build.js
@@ -1,13 +1,13 @@
-const fs = require("fs")
-const exec = require('child_process').exec;
-const open = require('open');
+// const fs = require("fs")
+// const exec = require('child_process').exec;
+// const open = require('open');
 
 
-let template = fs.readFileSync("entire.html", "utf-8")
+// let template = fs.readFileSync("entire.html", "utf-8")
 
-fs.writeFileSync('build/result.html', template)
+// fs.writeFileSync('build/result.html', template)
 
-exec("npm build")
-exec("http-server", {
-    cwd: "./build"
-})
+// exec("npm build")
+// exec("http-server", {
+//     cwd: "./build"
+// })
