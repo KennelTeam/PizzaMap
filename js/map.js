f91310fe35f871cfa48fcf0c5131200072a469bc
nebolax <alexey.nebolsin.jr@gmail.com>
2021-11-14 09:58:10 +0300

    added area stats label

--- js/map.js
+++ js/map.js
@@ -12,15 +12,20 @@ function polygonStatsPressed() {
         type: "Polygon",
     });
     map.addInteraction(draw);
-    draw.on("drawend", _ => {
+    draw.on("drawend", event => {
+        label_cords = event.feature.getGeometry().getCoordinates()[0][0]
         hasSelection = true
         map.removeInteraction(draw)
+
+        // insert here function to call bigdata analysisS
+        show_legend(label_cords, {})
     })
 }
 
 function removePolygonStats() {
     hasSelection = false
     polygonSource.clear()
+    popupDOM.style.display = "none"
 }
 
 function changeCity(cityPoint) {
@@ -47,11 +52,36 @@ function setData(orders_data) {
 const polygonSource = new ol.source.Vector({ wrapX: false });
 const heatmapSource = new ol.source.Vector({})
 
-var hasSelection = false
+var hasSelection
 var draw
 var map
+var popup
+var popupDOM
+
+// point has to be array of floats, not string
+function show_legend(point_loc, data) {
+    // ol3_sprint_location = ol.proj.transform(point, 'EPSG:4326', 'EPSG:3857');
+    console.log(point_loc)
+    popupDOM.style.display = "block"
+
+    popup_pos = map.getPixelFromCoordinate(point_loc)
+    console.log(popup_pos)
+    popup_width = document.getElementById("popup").offsetWidth
+    console.log(popup_width)
+    popup_pos[0] -= popup_width + 10
+    popup_pos[1] -= 10
+    cord_popup_pos = map.getCoordinateFromPixel(popup_pos)
+    popup.setPosition(cord_popup_pos);
+
+
+    $("#areaOrdersCount").text(data.ordersCount)
+    $("#areaLatesCount").text(data.latesCount)
+    $("#areaDeliveryTime").text(data.deliveryTime)
+    $("#areaEarnings").text(data.earnings)
+}
 
 function setup_OLMap() {
+    hasSelection = false
     console.log("Setuping")
     map = new ol.Map({
         // controls: ol.control.defaults().extend([panel]),
@@ -76,4 +106,15 @@ function setup_OLMap() {
             zoom: 11,
         })
     });
+
+    popupDOM = document.getElementById('popup')
+    popup = new ol.Overlay({
+        element: popupDOM
+    });
+    map.addOverlay(popup);
+
+    ol3_sprint_location = ol.proj.transform([39.954962, 57.651345], 'EPSG:4326', 'EPSG:3857');
+    popup.setPosition(ol3_sprint_location);
+
+    popupDOM.style.display = "none"
 }
